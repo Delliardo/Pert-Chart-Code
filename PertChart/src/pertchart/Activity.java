784a0630f9d985bc5a076fee61a29b57fb177322
zacsme <zaudette14@gmail.com>
2015-03-23 19:10:53 -0400

    Added getters and setters

--- PertChart/src/pertchart/Activity.java
+++ PertChart/src/pertchart/Activity.java
@@ -31,4 +31,132 @@ public class Activity {
     private float freeSlack;
     private float interferingSlack;
     private float independentSlack;
+    
+    public String getActivityId() {
+        return this.activityId;
+    }
+    
+    public void setActivityId(String id) {
+        this.activityId = id;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public void setDescription(String d) {
+        this.description = d;
+    }
+    
+    public Double getOptimisticTime() {
+        return this.optimisticTime;
+    }
+    
+    public void setOptimisticTime(Double t) {
+        this.optimisticTime = t;
+    }
+    
+    public Double getMostLikelyTimeTime() {
+        return this.mostLikelyTimeTime;
+    }
+    
+    public void setMostLikelyTimeTime(Double t) {
+        this.mostLikelyTimeTime = t;
+    }
+    
+    public Double getPessimisticTime() {
+        return this.pessimisticTime;
+    }
+    
+    public void setPessimisticTime(Double t) {
+        this.pessimisticTime = t;
+    }
+    
+    public Double getDuration() {
+        return this.duration;
+    }
+    
+    public void setDuration(Double t) {
+        this.duration = t;
+    }
+    
+    public Double getDeadline() {
+        return this.deadline;
+    }
+    
+    public void setDeadline(Double t) {
+        this.deadline = t;
+    }
+    
+    public Double getEarliestStartTime() {
+        return this.earliestStartTime;
+    }
+    
+    public void setEarliestStartTime(Double t) {
+        this.earliestStartTime = t;
+    }
+    
+    public Double getLatestStartTime() {
+        return this.latestStartTime;
+    }
+    
+    public void setLatestStartTime(Double t) {
+        this.latestStartTime = t;
+    }
+    
+    public Double getEarliestCompletionTime() {
+        return this.earliestCompletionTime;
+    }
+    
+    public void setEarliestCompletionTime(Double t) {
+        this.earliestCompletionTime = t;
+    }
+    
+    public Double getExpectedCompletionTime() {
+        return this.expectedCompletionTime;
+    }
+    
+    public void setExpectedCompletionTime(Double t) {
+        this.expectedCompletionTime = t;
+    }
+            
+    public Double getLatestCompletionTime() {
+        return this.latestCompletionTime;
+    }
+    
+    public void setLatestCompletionTime(Double t) {
+        this.latestCompletionTime = t;
+    }
+    
+    public float getTotalSlack() {
+        return this.totalSlack;
+    }
+    
+    public void setTotalSlack(float s) {
+        this.totalSlack = s;
+    }
+    
+    public float getFreeSlack() {
+        return this.freeSlack;
+    }
+    
+    public void setFreeSlack(float s) {
+        this.freeSlack = s;
+    }
+    
+    public float getInterferingSlack() {
+        return this.interferingSlack;
+    }
+    
+    public void setInterferingSlack(float s) {
+        this.interferingSlack = s;
+    }
+    
+    public float getIndependentSlack() {
+        return this.independentSlack;
+    }
+    
+    public void setIndependentSlack(float s) {
+        this.independentSlack = s;
+    }
 }
--- PertChart/src/pertchart/Project.java
+++ PertChart/src/pertchart/Project.java
@@ -17,4 +17,60 @@ public class Project {
     private double specifiedDeadline;
     private double expectedDuration;
     private double expectedDurationStandardDeviation;
+    
+    public Double getDuration() {
+        return this.duration;
+    }
+    
+    public void setDuration(Double t) {
+        this.duration = t;
+    }
+    
+    public Double getProbabilityOnTime() {
+        return this.probabilityOnTime;
+    }
+    
+    public void setProbabilityOnTime(Double t) {
+        this.probabilityOnTime = t;
+    }
+    
+    public Double getProbabilityByDeadline() {
+        return this.probabilityByDeadline;
+    }
+    
+    public void setProbabilityByDeadline(Double t) {
+        this.probabilityByDeadline = t;
+    }
+    
+    public Float getTotalMinSlack() {
+        return this.totalMinSlack;
+    }
+    
+    public void setTotalMinSlack(Float s) {
+        this.totalMinSlack = s;
+    }
+    
+    public Double getSpecifiedDeadline() {
+        return this.specifiedDeadline;
+    }
+    
+    public void setSpecifiedDeadline(Double t) {
+        this.specifiedDeadline = t;
+    }
+    
+    public Double getExpectedDuration() {
+        return this.expectedDuration;
+    }
+    
+    public void setExpectedDuration(Double t) {
+        this.expectedDuration = t;
+    }
+    
+    public Double getExpectedDurationStandardDeviation() {
+        return this.expectedDurationStandardDeviation;
+    }
+    
+    public void setExpectedDurationStandardDeviation(Double t) {
+        this.expectedDurationStandardDeviation = t;
+    }
 }
