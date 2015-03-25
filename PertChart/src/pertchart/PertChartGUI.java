febd20d3edd41729b3574e330a4b8c936fb9df47
Dan Vanuch <dan.vanuch@yahoo.com>
2015-03-25 13:46:51 -0400

    Updated the GUI

--- PertChart/src/pertchart/PertChartGUI.java
+++ PertChart/src/pertchart/PertChartGUI.java
@@ -5,160 +5,157 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
+public class PertChartGUI extends JFrame implements ActionListener {
 
-public class PertChartGUI extends JFrame implements ActionListener
-{
     //Data panel
-    private JPanel mainPanel, tablePanel, resourcePanel;
-    private JLabel TotalResources;
-    private JTextField Resources;
-
-    private JButton single, runSim;
-    
+    private JPanel projectPanel, tablePanel, resourcePanel, graph;
+    private JLabel TotalResources, lbl_Early, lbl_Middle, lbl_Late, lbl_Duration_Interval;
+    private JTextField Resources, early, middle, late, durationInterval;
+    private Calculations calculations;
+    private JButton runSim, simulationGraph;
+    private double earlyValue, middleValue, lateValue, durationValue, resourcesValue;
     private DefaultTableModel preTableModel;
     private JTable preTable;
-    private String[] columns = {"Activity ID", "Description", "Earliest", "Middle", "Latest", "Predecessors", "Resources"};
-    private String[][] data = {{"","","","","","",""},{"","","","","","",""},{"","","","","","",""}};
-    
-    
-    PertChartGUI()
-    {
-        super("Shipment Input");
-        Container c = getContentPane();
+    private String[] columns = {"Activity ID", "Duration", "Predecessors", "Resources"};
+    private String[][] data = {{"", "", "", ""}};
 
-        //Create main panel
-        mainPanel = new JPanel();
-        single = new JButton("Calculate Single Table");
-        single.addActionListener(this);
-        single.setToolTipText("Shows a single table.");
-        runSim = new JButton("Run Simulation");
-        runSim.addActionListener(this);
-        runSim.setToolTipText("Calculates an average completion time.");
-        mainPanel.add(single);
-        mainPanel.add(runSim);
-        c.add(mainPanel, BorderLayout.SOUTH);
-        
-        
-        
+    PertChartGUI() {
+        super("STARC");
         //Create table panel
         tablePanel = new JPanel();
         preTableModel = new DefaultTableModel(data, columns);
         preTable = new JTable(preTableModel);
         preTable.setPreferredScrollableViewportSize(new Dimension(500, 200));
         preTable.setFillsViewportHeight(false);
-        preTable.addKeyListener(new KeyListener()
-            {
-                public void keyPressed(KeyEvent e)
-                {
-                    if (e.getKeyCode()==KeyEvent.VK_ENTER)
-                    {
-                        preTableModel.addRow(new Object[]{"", ""});
-                    } 
+        preTable.addKeyListener(new KeyListener() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    preTableModel.addRow(new Object[]{"", "", "", ""});
                 }
-                public void keyReleased(KeyEvent e) { }
-                public void keyTyped(KeyEvent e) { } 
-            });
+            }
+
+            public void keyReleased(KeyEvent e) {
+            }
+
+            public void keyTyped(KeyEvent e) {
+            }
+        });
         tablePanel.add(new JScrollPane(preTable));
-        c.add(tablePanel, BorderLayout.NORTH);
-        
+        add(tablePanel, BorderLayout.NORTH);
+
         //Create resource panel
         resourcePanel = new JPanel();
-        TotalResources = new JLabel("Total Resources:");
-        Resources = new JTextField(10);
-        resourcePanel.add(TotalResources);
-        resourcePanel.add(Resources);
-        c.add(resourcePanel, BorderLayout.CENTER);
-        
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(200,350);
-        pack();
+        lbl_Early = new JLabel("Early");
+        lbl_Middle = new JLabel("Middle");
+        lbl_Late = new JLabel("Late");
+        lbl_Duration_Interval = new JLabel("Duration Interval");
+        durationInterval = new JTextField(5);
+        early = new JTextField(5);
+        middle = new JTextField(5);
+        late = new JTextField(5);
+        resourcePanel.add(lbl_Early);
+        resourcePanel.add(early);
+        resourcePanel.add(lbl_Middle);
+        resourcePanel.add(middle);
+        resourcePanel.add(lbl_Late);
+        resourcePanel.add(late);
+        resourcePanel.add(lbl_Duration_Interval);
+        resourcePanel.add(durationInterval);
+
+        add(resourcePanel, BorderLayout.CENTER);
+
+        //Create project info panel
+        projectPanel = new JPanel();
+        TotalResources = new JLabel("Total Resources:");
+        Resources = new JTextField(5);
+        projectPanel.add(TotalResources);
+        projectPanel.add(Resources);
+        runSim = new JButton("Run Simulation");
+        runSim.addActionListener(this);
+        runSim.setToolTipText("Calculates an average completion time.");
+        projectPanel.add(runSim);
+        simulationGraph = new JButton("Show Graph");
+        simulationGraph.addActionListener(this);
+        simulationGraph.setVisible(false);
+        projectPanel.add(simulationGraph);
+
+        add(projectPanel, BorderLayout.SOUTH);
+//        SimulatorGraph graph = new SimulatorGraph();
+//        add(graph);
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(540, 350);
+        //  pack();
         setVisible(true);
     }
-    
-    
+
     @Override
     /**
      * Detect which button the user just pressed.
      */
-    public void actionPerformed(ActionEvent e)
-    {
-        if (e.getSource() == single)
-        {
-            doSingleRun();
-        }
-        
-        else if (e.getSource() == runSim)
-        {
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == runSim) {
             runSimulator();
+        } else if (e.getSource() == simulationGraph) {
+            JFrame frame = new JFrame();
+            frame.setTitle("Simulation Graph");
+            frame.setResizable(true);
+            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setSize(500,350);
+            graph = new SimulatorGraph();
+            frame.add(graph);
+
         }
+
     }//actionPerformed
-    
-    
+
     /**
-     * Using the data from the preTable and Resource textbox, this method will call
-     * the appropriate method to calculate a single Pert table.  The called method
-     * should return the results in a 2d String array.  The 2D string array will
-     * be used in a new table to display the results.
+     * Using the data from the preTable and Resource textbox, this method will
+     * call the appropriate method to calculate a single Pert table. The called
+     * method should return the results in a 2d String array. The 2D string
+     * array will be used in a new table to display the results.
      */
-    public void doSingleRun()
-    {
-        try
-        {
-            int resNum = Integer.parseInt(Resources.getText());
-            
-            /**call method singRun(resNum, preTableModel)
-             * that method needs to return a 2d char array for table building.
-             */
-            
-            //Some test code for preTableModel data retrieval
-            for(int i = 0; i < preTableModel.getRowCount(); ++i)
-            {
-                for(int j = 0; j < preTableModel.getColumnCount(); ++j)
-                {
-                    System.out.print(preTableModel.getValueAt(i, j) + " ");
-                }
-                System.out.println("");
-            }
-            
-            String[][] tempData = {{"","","","","","",""},{"","","","","","",""},{"","","","","","",""}};
-            
-            SingleTableGUI gui = new SingleTableGUI(resNum, tempData);
+    public void runSimulator() {
+        try {
+            earlyValue = Double.parseDouble(this.early.getText());
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error in your Early time", "Error", JOptionPane.ERROR_MESSAGE);
         }
-        catch (NumberFormatException e)
-        {
-            JOptionPane.showMessageDialog(null, "Enter a number in the Total Resources field.", "Error", JOptionPane.ERROR_MESSAGE);
-        }  
-    }
-    
-    public void runSimulator()
-    {
-        try
-        {
-            int resNum = Integer.parseInt(Resources.getText());
-            
-            while(true)
-            {
-                try
-                {
-                    String input = JOptionPane.showInputDialog("Enter number of runs:");
-                    int numRuns = Integer.parseInt(input);
-                    break;
-                }
-                catch (Exception e)
-                {
-                    JOptionPane.showMessageDialog(null, "Use a number!", "Error", JOptionPane.ERROR_MESSAGE);
-                }
-            }
+
+        try {
+            middleValue = Double.parseDouble(this.middle.getText());
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error in your Middle time", "Error", JOptionPane.ERROR_MESSAGE);
         }
-        catch (Exception e)
-        {
+
+        try {
+            lateValue = Double.parseDouble(this.late.getText());
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error in your Late time", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        calculations = new Calculations(earlyValue, middleValue, lateValue, this.durationInterval.getText());
+
+        try {
+
+            for (int i = 0; i < preTableModel.getRowCount(); ++i) {
+
+                durationValue = Double.parseDouble((String) preTableModel.getValueAt(i, 1));
+                resourcesValue = Double.parseDouble((String) preTableModel.getValueAt(i, 3));
+
+                Activity activity = new Activity((String) preTableModel.getValueAt(i, 0), durationValue, (String) preTableModel.getValueAt(i, 2), resourcesValue);
+                calculations.activities.add(activity);
+
+            }
+
+            calculations.run();
+            System.out.println(calculations.getPrintedResults());
+            simulationGraph.setVisible(true);
+
+        } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Enter a number in the Total Resources field.", "Error", JOptionPane.ERROR_MESSAGE);
         }
-        
-        /**
-         * Call method simulation(resNum, preTableModel, numRuns)
-         * returns average project completion time.
-         */
     }
 }
