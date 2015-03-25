82f8d82683179acaac76a29a51480b60d2f1bf9c
Dan Vanuch <dan.vanuch@yahoo.com>
2015-03-25 15:11:23 -0400

    Updated the GUI with new Titles

--- PertChart/src/pertchart/PertChartGUI.java
+++ PertChart/src/pertchart/PertChartGUI.java
@@ -8,14 +8,15 @@ import javax.swing.table.*;
 public class PertChartGUI extends JFrame implements ActionListener {
 
     //Data panel
-    private JPanel projectPanel, tablePanel, resourcePanel, graph;
-    private JLabel TotalResources, lbl_Early, lbl_Middle, lbl_Late, lbl_Duration_Interval;
+    private JPanel projectPanel, tablePanel, resourcePanel, graph, outerPanel, northPanel;
+    private JLabel TotalResources, lbl_Early, lbl_Middle, lbl_Late, lbl_Duration_Interval, projectInfo;
     private JTextField Resources, early, middle, late, durationInterval;
     private Calculations calculations;
     private JButton runSim, simulationGraph;
     private double earlyValue, middleValue, lateValue, durationValue, resourcesValue;
     private DefaultTableModel preTableModel;
     private JTable preTable;
+    private Activity activity;
     private String[] columns = {"Activity ID", "Duration", "Predecessors", "Resources"};
     private String[][] data = {{"", "", "", ""}};
 
@@ -23,9 +24,11 @@ public class PertChartGUI extends JFrame implements ActionListener {
         super("STARC");
         //Create table panel
         tablePanel = new JPanel();
+        JLabel activityInfo = new JLabel("Activity Information");
+        tablePanel.add(activityInfo);
         preTableModel = new DefaultTableModel(data, columns);
         preTable = new JTable(preTableModel);
-        preTable.setPreferredScrollableViewportSize(new Dimension(500, 200));
+        preTable.setPreferredScrollableViewportSize(new Dimension(500, 300));
         preTable.setFillsViewportHeight(false);
         preTable.addKeyListener(new KeyListener() {
             public void keyPressed(KeyEvent e) {
@@ -42,6 +45,14 @@ public class PertChartGUI extends JFrame implements ActionListener {
         });
         tablePanel.add(new JScrollPane(preTable));
         add(tablePanel, BorderLayout.NORTH);
+
+        outerPanel = new JPanel();
+
+        northPanel = new JPanel();
+        projectInfo = new JLabel("Project Information");
+        northPanel.add(projectInfo);
+
+        outerPanel.add(northPanel, BorderLayout.NORTH);
 
         //Create resource panel
         resourcePanel = new JPanel();
@@ -63,7 +74,7 @@ public class PertChartGUI extends JFrame implements ActionListener {
         resourcePanel.add(lbl_Duration_Interval);
         resourcePanel.add(durationInterval);
 
-        add(resourcePanel, BorderLayout.CENTER);
+        outerPanel.add(resourcePanel, BorderLayout.CENTER);
 
         //Create project info panel
         projectPanel = new JPanel();
@@ -78,14 +89,16 @@ public class PertChartGUI extends JFrame implements ActionListener {
         simulationGraph = new JButton("Show Graph");
         simulationGraph.addActionListener(this);
         simulationGraph.setVisible(false);
-       // projectPanel.add(simulationGraph);
+        // projectPanel.add(simulationGraph);
 
-        add(projectPanel, BorderLayout.SOUTH);
+        outerPanel.add(projectPanel, BorderLayout.SOUTH);
 //        SimulatorGraph graph = new SimulatorGraph();
 //        add(graph);
 
+        add(outerPanel, BorderLayout.CENTER);
+
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setSize(540, 350);
+        setSize(540, 500);
         //  pack();
         setVisible(true);
     }
@@ -101,16 +114,16 @@ public class PertChartGUI extends JFrame implements ActionListener {
         } else if (e.getSource() == simulationGraph) {
             /*
             
-            This code can be used later to display the graph
-            JFrame frame = new JFrame();
-            frame.setTitle("Simulation Graph");
-            frame.setResizable(true);
-            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            frame.setVisible(true);
-            frame.setSize(500,350);
-            graph = new SimulatorGraph();
-            frame.add(graph);
-            */
+             This code can be used later to display the graph
+             JFrame frame = new JFrame();
+             frame.setTitle("Simulation Graph");
+             frame.setResizable(true);
+             frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+             frame.setVisible(true);
+             frame.setSize(500,350);
+             graph = new SimulatorGraph();
+             frame.add(graph);
+             */
 
         }
 
@@ -125,41 +138,36 @@ public class PertChartGUI extends JFrame implements ActionListener {
     public void runSimulator() {
         try {
             earlyValue = Double.parseDouble(this.early.getText());
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error in your Early time", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-
-        try {
             middleValue = Double.parseDouble(this.middle.getText());
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error in your Middle time", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-
-        try {
             lateValue = Double.parseDouble(this.late.getText());
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error in your Late time", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error in your time estimates", "Error", JOptionPane.ERROR_MESSAGE);
         }
+
         calculations = new Calculations(earlyValue, middleValue, lateValue, this.durationInterval.getText());
 
-        try {
-
-            for (int i = 0; i < preTableModel.getRowCount(); ++i) {
+        for (int i = 0; i < preTableModel.getRowCount(); ++i) {
+            try {
 
                 durationValue = Double.parseDouble((String) preTableModel.getValueAt(i, 1));
+                System.out.println(durationValue);
                 resourcesValue = Double.parseDouble((String) preTableModel.getValueAt(i, 3));
+                System.out.println(resourcesValue);
+                System.out.println((String) preTableModel.getValueAt(i, 0));
+                System.out.println((String) preTableModel.getValueAt(i, 2));
+                
 
-                Activity activity = new Activity((String) preTableModel.getValueAt(i, 0), durationValue, (String) preTableModel.getValueAt(i, 2), resourcesValue);
-                calculations.activities.add(activity);
-
+                activity = new Activity((String) preTableModel.getValueAt(i, 0), durationValue,(String) preTableModel.getValueAt(i, 2) , resourcesValue);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Activity information has been entered incorrectly", "Error", JOptionPane.ERROR_MESSAGE);
             }
+            calculations.activities.add(activity);
 
-            calculations.run();
-            System.out.println(calculations.getPrintedResults());
-            simulationGraph.setVisible(true);
-
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Enter a number in the Total Resources field.", "Error", JOptionPane.ERROR_MESSAGE);
         }
+
+        calculations.run();
+        System.out.println(calculations.getPrintedResults());
+        //simulationGraph.setVisible(true);
+
     }
 }
