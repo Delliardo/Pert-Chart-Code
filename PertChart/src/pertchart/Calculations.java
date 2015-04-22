6680a250c7439815e75145e5b79aaa750fecebef
zacsme <zaudette14@gmail.com>
2015-04-21 21:39:32 -0400

    Added support for resources. Added final calculations and simulation loop. Added functions to return the results.

--- PertChart/src/pertchart/Calculations.java
+++ PertChart/src/pertchart/Calculations.java
@@ -6,6 +6,11 @@
 package pertchart;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,27 +19,81 @@ import java.util.ArrayList;
 public class Calculations {
     
     protected ArrayList<Activity> activities;
-    private  final Project project;
+    private Project project;
     private final ArrayList<Activity> completedActvities =  new ArrayList();
     private final ArrayList<Activity> activitiesQueue = new ArrayList();
+    private Random random = new Random();
+    private ArrayList<Activity> activityResults;
+    private HashMap<String, Double[]> activityMap = new HashMap<>();
+    private ArrayList<Activity> simActivities;
+    private String timeUnit;
+    private double totalResources;
+    private double averageProjectCompletionTime = 0.0;
+    private double simLength = 1000.0;
     
-    public Calculations(double a, double m, double b, String di) {
-        this.project = new Project(a, m, b, di);
+    public Calculations(String di, int tr) {
+        this.timeUnit = di;
+        this.totalResources = tr;
+        this.project = new Project(di, tr);
         this.activities = new ArrayList();
     }
     
     public void run() {
         determinePredecessors();
         determineSuccessors();
-        calculateProjectCompletionTime();
-        getPrintedResults();
+        generateHashMap();
+        
+        // copy the activities and project to use for final results
+        this.activityResults = this.activities;
+        
+        // run simulation
+        for (int i = 0; i < simLength; i++) {
+            this.simActivities = this.activities;
+            calculateActvityCompletionTime();
+            calculateProjectCompletionTime();
+        }
+        
+        // calculate the final results
+        Iterator it = this.activityMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry a = (Map.Entry)it.next();
+            Double[] d = (Double[])a.getValue();
+            d[0] = d[0] / this.simLength;
+            d[1] = d[1] / this.simLength;
+            d[2] = d[2] / this.simLength;
+            a.setValue(d);
+            it.remove();
+        }
+        
+        this.averageProjectCompletionTime = this.averageProjectCompletionTime / this.simLength;
+    }
+    
+    public void generateHashMap() {
+        for (Activity a: this.activities) {
+            Double[] d = {0.0, 0.0, 0.0};
+            this.activityMap.put(a.getActivityId(), d);
+        }
     }
     
     public Activity getActivityById(String s) {
         Activity a = null;
+        for (Activity b: this.activityResults) {
+            if (s.equalsIgnoreCase(b.getActivityId()) == false) {
+               continue;
+            }
+            else {
+                a = b;
+                break;
+            }
+        }
+        return a;
+    }
+    
+    public Activity getActivityResultById(String s) {
+        Activity a = null;
         for (Activity b: this.activities) {
             if (s.equalsIgnoreCase(b.getActivityId()) == false) {
-               // do nothing
+               continue;
             }
             else {
                 a = b;
@@ -73,51 +132,94 @@ public class Calculations {
         }
     }
     
+    public void calculateActvityCompletionTime() {
+        for (Activity a: this.simActivities) {
+            double amb = (a.getB() + (4.0 * a.getM()) + a.getA()) / 6.0;
+            double lateTimeRange = amb;
+            double earlyTimeRange = amb;
+            if (amb < a.getB()) {
+                lateTimeRange = (amb + a.getB()) / 2.0;
+            }
+            if (amb > a.getA()) {
+                earlyTimeRange = (amb + a.getA()) / 2.0;
+            }
+            double range = lateTimeRange - earlyTimeRange;
+            double scaled = random.nextDouble() * range;
+            double expectedTime = scaled + earlyTimeRange;
+            a.setExpectedTime(expectedTime);
+            
+            // calculate variance
+            double v = Math.pow((a.getB() - a.getA()), 2) / 36.0;
+            a.setAverageVariance(v);
+        }
+    }
+    
     public void calculateProjectCompletionTime() {
         double projectCompletionTime = 0.0;
         // push all activities that have no predecessors into the queue
-        for (Activity a: this.activities) {
+        for (Activity a: this.simActivities) {
             ArrayList<Activity> pList  = a.getPredecessors();
             if (pList.size() > 0) {
-                // do nothing
+                continue;
             }
             else {
-                a.setEarliestStartTime(0.0);
+                a.setStartTime(0.0);
                 activitiesQueue.add(a);
             }
         }
-        
+        int currentActivityIndex = 0;
         while (activitiesQueue.size() > 0) {
-            Activity a = activitiesQueue.remove(0);
-            double completionTime = a.getEarliestStartTime() + a.getDuration();
-            a.setEarliestCompletionTime(completionTime);
-            completedActvities.add(a);
-            
-            // check if this activities completion time is higher than the project's
-            if (completionTime > projectCompletionTime) {
-                projectCompletionTime = completionTime;
-            }
+            Activity a = activitiesQueue.get(currentActivityIndex);
+            while (this.totalResources > a.getResources()) {
+                if (currentActivityIndex <= 0) {
+                    currentActivityIndex = 0;
+                }
+                else {
+                    currentActivityIndex--;
+                }
+                double completionTime = a.getStartTime() + a.getExpectedTime();
+                a.setCompletionTime(completionTime);
+                completedActvities.add(a);
+                
+                // update our hash map for the final results
+                Double[] d = this.activityMap.get(a.getActivityId());
+                d[0] = d[0] + a.getAverageVariance();
+                d[1] = d[1] + a.getStartTime();
+                d[2] = d[2] + completionTime;
+                this.activityMap.put(a.getActivityId(), d);
 
-            // push next activities into queue whose successors have finished
-            for (Activity successor: a.getSuccessors()) {
-                ArrayList<Activity> predecessorList = successor.getPredecessors();
-                int pListL = predecessorList.size();
-                int completedPredecessors = 0;
-                for (int i = 0; i < pListL; i++) {
-                    Activity predecessor = predecessorList.get(i);
-                    if (activityHasBeenCompleted(predecessor.getActivityId())) {
-                        completedPredecessors = completedPredecessors + 1;
+                // check if this activities completion time is higher than the project's
+                if (completionTime > projectCompletionTime) {
+                    projectCompletionTime = completionTime;
+                }
+
+                // push next activities into queue whose successors have finished
+                for (Activity successor: a.getSuccessors()) {
+                    ArrayList<Activity> predecessorList = successor.getPredecessors();
+                    int pListL = predecessorList.size();
+                    int completedPredecessors = 0;
+                    for (int i = 0; i < pListL; i++) {
+                        Activity predecessor = predecessorList.get(i);
+                        if (activityHasBeenCompleted(predecessor.getActivityId())) {
+                            completedPredecessors = completedPredecessors + 1;
+                        }
+                    }
+
+                    // if all predecessors have been completed then push the successor into the queue
+                    if (completedPredecessors == pListL) {
+                        successor.setStartTime(a.getCompletionTime());
+                        activitiesQueue.add(successor);
                     }
                 }
-
-                // if all predecessors have been completed then push the successor into the queue
-                if (completedPredecessors == pListL) {
-                    successor.setEarliestStartTime(a.getEarliestCompletionTime());
-                    activitiesQueue.add(successor);
-                }
+            }
+            currentActivityIndex++;
+            if (currentActivityIndex > activitiesQueue.size() - 1) {
+                JOptionPane.showMessageDialog(null, "There are not enough resources for this project.");
+                break;
             }
         }
-        project.setExpectedDuration(projectCompletionTime);
+        this.averageProjectCompletionTime = this.averageProjectCompletionTime + projectCompletionTime;
+        project.setCompletionTime(projectCompletionTime);
     }
     
     public boolean activityHasBeenCompleted(String id) {
@@ -126,7 +228,7 @@ public class Calculations {
         for (int i = 0; i < len; i++) {
             Activity completedActivity = completedActvities.get(i);
             if (completedActivity.getActivityId().equalsIgnoreCase(id) == false) {
-                // do nothing
+                continue;
             }
             else {
                 completed = true;
@@ -136,8 +238,11 @@ public class Calculations {
         return completed;
     }
     
-    public String getPrintedResults() {
-        String output = "Project completion time: " + project.getExpectedDuration() + " " + project.getDurationInterval();
-        return output;
+    public HashMap<String, Double[]> getActivityResults() {
+        return this.activityMap;
+    }
+    
+    public Double getProjectCompletionTime() {
+        return this.averageProjectCompletionTime;
     }
 }
